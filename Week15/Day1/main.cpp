051fec629536496295d85f732c24b7242b8ff284
Sayeem-Ahmed <152946787+Sayeem-Ahmed850@users.noreply.github.com>
2025-02-10 21:32:11 +0600

    Add files via upload

---
+++ Week15/Day1/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pi pair<int, int>
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define nl '\n'
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main()
+{
+    FIO;
+    
+    return 0;
+}
